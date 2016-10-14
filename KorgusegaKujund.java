ac67e5749794fc734b793a729767785a41a12298
Jaak Rooden <jaak.rooden@tigu.hk.tlu.ee>
2016-10-14 12:56:23 +0300

    Korgusega kujund

---
+++ KorgusegaKujund.java
@@ -0,0 +1,13 @@
+public abstract class KorgusegaKujund{
+	double korgus;
+	public KorgusegaKujund(double uusKorgus){
+		korgus=uusKorgus;
+	}
+	public abstract double pohjaPindala();
+	public double ruumala(){
+		return pohjaPindala()*korgus;
+		
+		
+	}
+	
+}

553826dc6f4436f4d22bc7a3174aa4bf91dfa66d
Jaak Rooden <jaak.rooden@tigu.hk.tlu.ee>
2016-10-14 13:03:12 +0300

    Silinder

---
+++ Silinder.java
@@ -0,0 +1,11 @@
+public class Silinder extends KorgusegaKujund{
+	double r;
+	public Silinder(double korgus, double raadius){
+		super(korgus);
+		r=raadius;
+	}
+	public double pohjaPindala(){
+		return 3.14*r*r;
+		
+	}
+}
